000dde5160be7ffc8e7784665b2e3204fd3be0be
Muhammad Boudad <mohammeditech7@gmail.com>
2017-12-09 10:47:13 +0100

    Add register page

--- Carpooling/README.md
+++ Carpooling/README.md
@@ -4,3 +4,14 @@
 
 * Add landing page
 * Add header and footer (add navbar to header)
+
+Each driver profile has:
+
+* First and last name
+* Profile picture
+* Rating
+
+## Create Sign up
+
+* Add sign up page
+* Setup sign up POST route
--- Carpooling/app.js
+++ Carpooling/app.js
@@ -5,10 +5,27 @@ var express     = require("express"),
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+var drivers = [
+    {firstName: "Mohammed", lastName: "Boudad"},
+    {firstName: "Tahar", lastName: "Boudad"}
+];
+
+// Landing page
 app.get("/", function(req, res) {
     res.render('landing.ejs');
 });
 
+// INDEX - Show all drivers
+app.get("/drivers", function(req, res) {
+    res.render("drivers", {drivers: drivers});
+});
+
+// REGISTER - show sign up page
+app.get("/register", function(req ,res) {
+    res.render("register");
+});
+
+// Starting app
 app.listen(8088, function() {
     console.log("Serving your app...");
 });
